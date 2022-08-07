d787c7013681f3fcaddddb64aa2690b4e224ee95
tristanpank <tristanpank@gmail.com>
2022-08-07 16:08:45 -0400

    Finish CSS Styling

---
+++ script.js
@@ -0,0 +1,20 @@
+let firstName = document.getElementById('first-name');
+let lastName = document.getElementById('last-name');
+let email = document.getElementById('email');
+let phoneNumber = document.getElementById('phone-number');
+let password = document.getElementById('password');
+let confirmPassword = document.getElementById('confirm-password');
+let submitButton = document.getElementById('create-account');
+
+
+
+
+
+
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(email);
+// console.log(phoneNumber);
+// console.log(password);
+// console.log(confirmPassword);
