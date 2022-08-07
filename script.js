22ecefb51edadb60e835a77bf419896f012b9dc0
tristanpank <tristanpank@gmail.com>
2022-08-07 16:35:38 -0400

    Add Confirm Password Check
    Used javascript to apply a check to the confirm password input.
    If passwords don't match, a red outline is applied and submit
    button is unable to submit the form.

--- script.js
+++ script.js
@@ -6,6 +6,29 @@ let password = document.getElementById('password');
 let confirmPassword = document.getElementById('confirm-password');
 let submitButton = document.getElementById('create-account');
 
+function checkPassword() {
+    if (password.value === confirmPassword.value) {
+        confirmPassword.className = 'valid';
+        submitButton.type = 'submit';
+        console.log(submitButton);
+    } else {
+        confirmPassword.className = 'invalid';
+        submitButton.type = 'button';
+        console.log(submitButton);
+    }
+};
+
+confirmPassword.addEventListener('keyup', checkPassword);
+
+password.addEventListener('keyup', checkPassword);
+
+// submitButton.addEventListener('click', () => {
+//     if (checkPassword() === true) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// });
 
 
 
